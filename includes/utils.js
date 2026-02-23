2046257dc5611c1ddf7f665404d2e26897fe1e3c
Irfana_Khan <irfana.khan@bilvantis.io>
2026-02-23 11:31:08 +0000

    dataform project changes added

---
+++ includes/utils.js
@@ -0,0 +1,19 @@
+function auditStart(model) {
+  return `
+    INSERT INTO audit.dataform_audit_log
+    VALUES ("${model}", "\${dataform.runId()}", "STARTED", CURRENT_TIMESTAMP(), NULL, NULL);
+  `;
+}
+
+function auditEnd(model) {
+  return `
+    UPDATE audit.dataform_audit_log
+    SET status="SUCCESS",
+        end_time=CURRENT_TIMESTAMP(),
+        row_count=(SELECT COUNT(*) FROM \${self()})
+    WHERE model_name="${model}"
+      AND run_id="\${dataform.runId()}";
+  `;
+}
+
+module.exports = { auditStart, auditEnd };
